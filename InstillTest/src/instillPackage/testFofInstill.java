b564a6d8bc50029a7bcb74f9e1c163c3bdffb7b8
godsfavournwoko <godsfavournwoko@192.168.100.39>
2024-01-20 01:47:41 +0100

    2nd commit

--- InstillTest/src/instillPackage/testFofInstill.java
+++ InstillTest/src/instillPackage/testFofInstill.java
@@ -12,9 +12,14 @@ public class testFofInstill {
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		
+		//This sets up the updated chromedriver
 		WebDriverManager.chromedriver().setup();
+		
+		//This instantiates and object of Chromedriver
 		WebDriver driver = new ChromeDriver();
 		
+		
+		//This calls the url, this can be parameterized if a frame work was used
 		driver.get("https://www.canva.com/en_gb/");
 		
 
@@ -32,7 +37,7 @@ public class testFofInstill {
 		driver.findElement(By.cssSelector("input[autocomplete*='name']")).sendKeys("Gman");
 		driver.findElement(By.xpath("//span[text()='Create your account']")).click();
 		
-		
+		//This retrieves a text to check that we have gotten to a page the requires a code input
 		String text = "Code";
 		String bodyText = driver.findElement(By.xpath("//span[text()='Code']")).getText();
 		Assert.assertTrue(bodyText.contains(text));
